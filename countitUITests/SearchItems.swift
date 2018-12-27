16ac63c50d49c0ee7f0726bc45e9f3662d8b0e80
David G <davidgrew@hotmail.co.uk>
2018-12-27 18:32:37 +0000

    [CI14] add search items test

--- countitUITests/SearchItems.swift
+++ countitUITests/SearchItems.swift
@@ -10,13 +10,9 @@ import XCTest
 
 class SearchItems: XCTestCase {
     
-    let ITEM_NAME_ONE = "TestItemNameOne"
-    let ITEM_NAME_TWO = "TestItemNameTwo"
-    
-    let ITEM_DESCRIPTION_ONE = "TestItemDescriptionOne"
-    let ITEM_DESCRIPTION_TWO = "TestItemDescriptionTwo"
-    
-    let ERROR_MESSAGE = "* Must provide a name"
+    let ITEM_NAME_A = "a"
+    let ITEM_NAME_AB = "ab"
+    let ITEM_NAME_ABC = "abc"
     
     override func setUp() {
         
@@ -28,23 +24,62 @@ class SearchItems: XCTestCase {
         
         app.navigationBars["COUNT IT"].buttons["Add"].tap()
         let nameField = app.otherElements["nameField"].textFields["fieldText"]
-        let descriptionField = app.otherElements["descriptionField"].textFields["fieldText"]
         
         nameField.tap()
-        nameField.typeText(ITEM_NAME_ONE)
-        descriptionField.tap()
-        descriptionField.typeText(ITEM_DESCRIPTION_ONE)
+        nameField.typeText(ITEM_NAME_A)
         app.navigationBars["ADD ITEM"].buttons["Save"].tap()
         
         app.navigationBars["COUNT IT"].buttons["Add"].tap()
         nameField.tap()
-        nameField.typeText(ITEM_NAME_TWO)
-        descriptionField.tap()
-        descriptionField.typeText(ITEM_DESCRIPTION_TWO)
+        nameField.typeText(ITEM_NAME_AB)
+        app.navigationBars["ADD ITEM"].buttons["Save"].tap()
+        
+        app.navigationBars["COUNT IT"].buttons["Add"].tap()
+        nameField.tap()
+        nameField.typeText(ITEM_NAME_ABC)
         app.navigationBars["ADD ITEM"].buttons["Save"].tap()
     }
     
     override func tearDown() {
     }
 
+    func testDynamicSearchForItems() {
+        
+        let app = XCUIApplication()
+        let itemTable = app.tables["ItemTable"]
+        let searchField = app.searchFields["Search items"]
+        
+        itemTable.swipeDown()
+        searchField.tap()
+        searchField.typeText("a")
+        
+        XCTAssertTrue(itemTable.cells.count == 3)
+        assertTable(cell: 0, is: ITEM_NAME_A)
+        assertTable(cell: 1, is: ITEM_NAME_AB)
+        assertTable(cell: 2, is: ITEM_NAME_ABC)
+        
+        searchField.typeText("b")
+        
+        XCTAssertTrue(itemTable.cells.count == 2)
+        assertTable(cell: 0, is: ITEM_NAME_AB)
+        assertTable(cell: 1, is: ITEM_NAME_ABC)
+        
+        searchField.typeText("c")
+        
+        XCTAssertTrue(itemTable.cells.count == 1)
+        assertTable(cell: 0, is: ITEM_NAME_ABC)
+        
+        searchField.typeText(XCUIKeyboardKey.delete.rawValue)
+        
+        XCTAssertTrue(itemTable.cells.count == 2)
+        assertTable(cell: 0, is: ITEM_NAME_AB)
+        assertTable(cell: 1, is: ITEM_NAME_ABC)
+        
+        searchField.typeText(XCUIKeyboardKey.delete.rawValue)
+        
+        XCTAssertTrue(itemTable.cells.count == 3)
+        assertTable(cell: 0, is: ITEM_NAME_A)
+        assertTable(cell: 1, is: ITEM_NAME_AB)
+        assertTable(cell: 2, is: ITEM_NAME_ABC)
+    }
 }
