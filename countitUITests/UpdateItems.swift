04fde30349c9e78e6f2de618b3e15bd6a3a37255
David G <davidgrew@hotmail.co.uk>
2019-01-05 12:07:33 +0000

    [CI17] write UI tests for updated target value

--- countitUITests/UpdateItems.swift
+++ countitUITests/UpdateItems.swift
@@ -19,6 +19,9 @@ class UpdateItems: XCTestCase {
     let ITEM_DESCRIPTION_ONE_NEW = "TestItemDescriptionOneNew"
     let ITEM_DESCRIPTION_TWO = "TestItemDescriptionTwo"
     
+    let ITEM_TARGET_VALUE_ONE = "10"
+    let ITEM_TARGET_VALUE_ONE_NEW = "20"
+    
     let ERROR_MESSAGE = "* Must provide a name"
     
     override func setUp() {
@@ -32,11 +35,15 @@ class UpdateItems: XCTestCase {
         app.navigationBars["COUNT IT"].buttons["Add"].tap()
         let nameField = app.otherElements["nameField"].textFields["fieldText"]
         let descriptionField = app.otherElements["descriptionField"].textFields["fieldText"]
+        let targetValue = app.textFields["targetValue"]
         
         nameField.tap()
         nameField.typeText(ITEM_NAME_ONE)
         descriptionField.tap()
         descriptionField.typeText(ITEM_DESCRIPTION_ONE)
+        descriptionField.typeText(String(XCUIKeyboardKey.return.rawValue))
+        targetValue.tap()
+        targetValue.typeText(ITEM_TARGET_VALUE_ONE)
         app.navigationBars["ADD ITEM"].buttons["Save"].tap()
     }
     
@@ -52,6 +59,7 @@ class UpdateItems: XCTestCase {
         
         let nameField = app.otherElements["nameField"].textFields["fieldText"]
         let descriptionField = app.otherElements["descriptionField"].textFields["fieldText"]
+        let targetValue = app.textFields["targetValue"]
         
         nameField.tap()
         nameField.clearAndEnterText(newText: ITEM_NAME_ONE_NEW)
@@ -64,6 +72,7 @@ class UpdateItems: XCTestCase {
         itemTable.cells["Cell0"].buttons["More Info"].tap()
         XCTAssertEqual(nameField.value as? String, ITEM_NAME_ONE_NEW, "item name not correct on form")
         XCTAssertEqual(descriptionField.value as? String, ITEM_DESCRIPTION_ONE, "item description not correct on form")
+        XCTAssertEqual(targetValue.value as? String, ITEM_TARGET_VALUE_ONE, "target value not correct in cell")
     }
     
     func testUpdateItemDescription() {
@@ -75,6 +84,7 @@ class UpdateItems: XCTestCase {
         
         let nameField = app.otherElements["nameField"].textFields["fieldText"]
         let descriptionField = app.otherElements["descriptionField"].textFields["fieldText"]
+        let targetValue = app.textFields["targetValue"]
         
         descriptionField.tap()
         descriptionField.clearAndEnterText(newText: ITEM_DESCRIPTION_ONE_NEW)
@@ -87,6 +97,32 @@ class UpdateItems: XCTestCase {
         itemTable.cells["Cell0"].buttons["More Info"].tap()
         XCTAssertEqual(nameField.value as? String, ITEM_NAME_ONE, "item name not correct on form")
         XCTAssertEqual(descriptionField.value as? String, ITEM_DESCRIPTION_ONE_NEW, "item description not correct on form")
+        XCTAssertEqual(targetValue.value as? String, ITEM_TARGET_VALUE_ONE, "target value not correct in cell")
+    }
+    
+    func testUpdateItemTargetValue() {
+        
+        let app = XCUIApplication()
+        let itemTable = app.tables["ItemTable"]
+        
+        itemTable.cells["Cell0"].buttons["More Info"].tap()
+        
+        let nameField = app.otherElements["nameField"].textFields["fieldText"]
+        let descriptionField = app.otherElements["descriptionField"].textFields["fieldText"]
+        let targetValue = app.textFields["targetValue"]
+        
+        targetValue.tap()
+        targetValue.clearAndEnterText(newText: ITEM_TARGET_VALUE_ONE_NEW)
+        
+        // verify name has not changed in item table
+        app.navigationBars[ITEM_NAME_ONE_UPPERCASE].buttons["Save"].tap()
+        XCTAssertEqual(itemTable.cells.element(boundBy: 0).staticTexts["itemName"].label, ITEM_NAME_ONE, "item name not correct in cell")
+        
+        // verify description has been updated on item form
+        itemTable.cells["Cell0"].buttons["More Info"].tap()
+        XCTAssertEqual(nameField.value as? String, ITEM_NAME_ONE, "item name not correct on form")
+        XCTAssertEqual(descriptionField.value as? String, ITEM_DESCRIPTION_ONE, "item description not correct on form")
+        XCTAssertEqual(targetValue.value as? String, ITEM_TARGET_VALUE_ONE_NEW, "target value not correct in cell")
     }
     
     func testCannotSaveIfNameDeleted() {
