ea6612ac4526976e3d29456ba0e082ba9392b843
David G <davidgrew@hotmail.co.uk>
2019-01-14 21:26:37 +0000

    [CI22] add custom message to assertions

--- countitUITests/assertions/UITestBaseAssertions.swift
+++ countitUITests/assertions/UITestBaseAssertions.swift
@@ -11,31 +11,52 @@ import XCTest
 
 extension UITestBase {
     
-    func assertTable(cell cellNumber: Int, is itemName: String) {
-        XCTAssertEqual(itemTable!.cells.element(boundBy: cellNumber).staticTexts[AI.PROGRESS_CELL_NAME].label, itemName, "item name not correct in cell")
+    func assertTable(cell cellNumber: Int, is expectedName: String, file: StaticString = #file, line: UInt = #line) {
+        let actualName = itemTable!.cells.element(boundBy: cellNumber).staticTexts[AI.PROGRESS_CELL_NAME].label
+        XCTAssertEqual(actualName, expectedName,
+                       "item name incorrect for cell \(cellNumber). Expected \(expectedName) but was \(actualName)",
+                       file: file, line: line)
     }
     
-    func assertItemForm(name: String?, description: String?, target: String?) {
-        if let nameUnwrapped = name {
-            XCTAssertEqual(nameField!.value as? String, nameUnwrapped, "item name not correct on form")
+    func assertItemForm(name: String?, description: String?, target: String?, file: StaticString = #file, line: UInt = #line) {
+        if let expectedName = name {
+            let actualName = nameField!.value as? String
+            XCTAssertEqual(expectedName, actualName,
+                           "item name incorrect. Expected \(expectedName) but was \(actualName ?? "")",
+                           file: file, line: line)
         }
-        if let descriptionUnwrapped = description {
-            XCTAssertEqual(descriptionField!.value as? String, descriptionUnwrapped, "item description not correct on form")
+        if let expectedDescription = description {
+            let actualDescription = descriptionField!.value as? String
+            XCTAssertEqual(actualDescription, expectedDescription,
+                           "item description incorrect. Expected \(expectedDescription) but was \(actualDescription ?? "")",
+                           file: file, line: line)
         }
-        if let targetUnwrapped = target {
-            XCTAssertEqual(targetField!.value as? String, targetUnwrapped, "target value not correct in cell")
+        if let expectedTarget = target {
+            let actualTarget = targetField!.value as? String
+            XCTAssertEqual(actualTarget, expectedTarget,
+                           "target value incorrect. Expected \(expectedTarget) but was \(actualTarget ?? "")",
+                           file: file, line: line)
         }
     }
     
-    func assertItemFormErrorIs(_ error: String) {
-        XCTAssertEqual(nameFieldError!.label, error)
+    func assertItemFormErrorIs(_ expectedError: String, file: StaticString = #file, line: UInt = #line) {
+        let actualError = nameFieldError!.label
+        XCTAssertEqual(actualError, expectedError,
+                       "error is incorrect. Expected \(expectedError) but was \(actualError)",
+                       file: file, line: line)
     }
     
-    func assertTableCountIs(_ count: Int) {
-        XCTAssertTrue(itemTable!.cells.count == count)
+    func assertTableCountIs(_ expectedCount: Int, file: StaticString = #file, line: UInt = #line) {
+        let actualCount = itemTable!.cells.count
+        XCTAssertTrue(expectedCount == actualCount,
+                      "table count incorrect. Expected \(expectedCount) but was \(actualCount)",
+                      file: file, line: line)
     }
     
-    func assertActivityCountFor(cell: Int, is activityCount: String) {
-        XCTAssertEqual(itemTable!.cells.element(boundBy: cell).staticTexts[AI.PROGRESS_CELL_ACTIVITY_COUNT].label, activityCount, "activityCount not correct")
+    func assertActivityCountFor(cell: Int, is expectedActivityCount: String, file: StaticString = #file, line: UInt = #line) {
+        let actualActivityCount = itemTable!.cells.element(boundBy: cell).staticTexts[AI.PROGRESS_CELL_ACTIVITY_COUNT].label
+        XCTAssertEqual(actualActivityCount, expectedActivityCount,
+                       "activityCount incorrect. Expected \(expectedActivityCount) but was \(actualActivityCount)",
+                       file: file, line: line)
     }
 }
